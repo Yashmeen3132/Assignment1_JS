06dee82ffe276343e4a2b76714f6e963b69ba023
Yashmeen3132 <132915194+Yashmeen3132@users.noreply.github.com>
2023-05-10 00:59:04 +0530

    Add files via upload

---
+++ Assignment/Assignment1_JS/TypeScript/variable.js
@@ -0,0 +1,7 @@
+var x = 10;
+console.log(x);
+//string
+var s1 = "working with typescript";
+var s2 = "yashmeen";
+console.log(s1);
+console.log(s2);
---
+++ Assignment/Assignment1_JS/TypeScript/variable.ts
@@ -0,0 +1,8 @@
+var x:number=10
+console.log(x)
+
+//string
+var s1:string = "working with typescript"
+var s2:string ="yashmeen"
+console.log(s1)
+console.log(s2)
